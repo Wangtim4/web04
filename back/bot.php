702e02efc418a1b5434504a564b58e938a6dd992
a2453540 <a2453540@gmail.com>
2023-06-04 02:30:21 +0800

    頁尾功能

--- back/bot.php
+++ back/bot.php
@@ -1,10 +1,18 @@
+
+<?php
+if(!empty($_POST)){
+    $bot=$Bot->find(1);
+    $bot['bot']=$_POST['bot'];
+    $Bot->save($bot);
+}
+?>
 <h2 class="ct">編輯頁尾版權區</h2>
-<form action="?" method="post">
+<form action="?do=bot" method="post">
     <div style="display: flex;">
         <div class="tt ct" style="width:30%">頁尾版權內容</div>
         <div class="pp" style="width:60%">
             <input style="width:90%" type="text"
-            name="bot" value="">
+            name="bot" value="<?=$Bot->find(1)['bot'];?>">
         </div>
     </div>
     <div class="ct">
