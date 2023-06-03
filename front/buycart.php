f388ea6bbbaccb60956a5ecc50406a4fe5b4ee9f
a2453540 <a2453540@gmail.com>
2023-06-04 01:34:58 +0800

    版型

---
+++ front/buycart.php
@@ -0,0 +1,20 @@
+<h2 class="ct">XXX購物車</h2>
+
+<table class="all">
+    <tr class="tt ct">
+        <th>編號</th>
+        <th>產品名稱</th>
+        <th>數量</th>
+        <th>庫存量</th>
+        <th>小計</th>
+        <th>刪除</th>
+    </tr>
+    <tr class="pp ct">
+        <td>編號</td>
+        <td>產品名稱</td>
+        <td>數量</td>
+        <td>庫存量</td>
+        <td>小計</td>
+        <td>刪除</td>
+    </tr>
+</table>
---
+++ front/main.php
@@ -0,0 +1,12 @@
+<h1>全部商品 > 流行皮件</h1>
+<div style="width: 90%; margin:auto; display:flex;" class="pp">
+    <div style="width: 40%; padding:1rem;">
+        <img style="width: 90%;"src="./img/0403.jpg" alt="">
+    </div>
+    <div style="width: 60%;">
+        <div class="tt">皮件</div>
+        <div>價格</div>
+        <div>規格</div>
+        <div>簡介</div>
+    </div>
+</div>
