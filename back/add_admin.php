cc6a7f9e7da0cc3353c6f2596401c0ee998798b6
a2453540 <a2453540@gmail.com>
2023-06-04 20:20:02 +0800

    完成新增管理者功能

--- back/add_admin.php
+++ back/add_admin.php
@@ -1,33 +1,39 @@
 <h2 class="ct">新增管理帳號</h2>
-<table class="all">
-    <tr>
-        <td class="ct tt">帳號</td>
-        <td class="pp"><input type="text" name="acc" id="acc"></td>
-    </tr>
-    <tr>
-        <td class="ct tt">密碼</td>
-        <td class="pp"><input type="password" name="pw" id="pw"></td>
-    </tr>
-    <tr>
-        <td class="ct tt">權限</td>
-        <td class="pp">
-
-            <div>
-                <input type="checkbox" name="pr[]" value="1">商品分類與管理</input>
-            </div>
-            <div>
-                <input type="checkbox" name="pr[]" value="2">訂單管理</input>
-            </div>
-            <div>
-                <input type="checkbox" name="pr[]" value="3">會員管理</input>
-            </div>
-            <div>
-                <input type="checkbox" name="pr[]" value="4">頁尾版權區管理</input>
-            </div>
-            <div>
-                <input type="checkbox" name="pr[]" value="5">最新消息管理</input>
-            </div>
-
-        </td>
-    </tr>
-</table>
+<form action="./api/add_admin.php" method="post">
+    <table class="all" id="addform">
+        <tr>
+            <td class="ct tt">帳號</td>
+            <td class="pp"><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="ct tt">密碼</td>
+            <td class="pp"><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td class="ct tt">權限</td>
+            <td class="pp">
+    
+                <div>
+                    <input type="checkbox" name="pr[]" value="1">商品分類與管理</input>
+                </div>
+                <div>
+                    <input type="checkbox" name="pr[]" value="2">訂單管理</input>
+                </div>
+                <div>
+                    <input type="checkbox" name="pr[]" value="3">會員管理</input>
+                </div>
+                <div>
+                    <input type="checkbox" name="pr[]" value="4">頁尾版權區管理</input>
+                </div>
+                <div>
+                    <input type="checkbox" name="pr[]" value="5">最新消息管理</input>
+                </div>
+    
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <button type="submit">新增</button>
+        <button type="reset">重置</button>
+    </div>
+</form>
