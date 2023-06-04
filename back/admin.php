1bfb4e3ba230f775ad6b437ab1c2b61a9240087e
a2453540 <a2453540@gmail.com>
2023-06-04 18:55:45 +0800

    新增管理帳號

--- back/admin.php
+++ back/admin.php
@@ -1,5 +1,5 @@
 <div class="ct">
-    <button>新增管理員</button>
+    <button onclick="location.href='?do=add_admin'">新增管理員</button>
 </div>
 <table class="all">
     <tr class="tt ct">
@@ -7,11 +7,29 @@
         <th>密碼</th>
         <th>管理</th>
     </tr>
-    <tr class="pp ct">
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
+    <?php
+    $rows = $Admin->all();
+    foreach ($rows as $row) {
+    ?>
+        <tr class="pp ct">
+            <td><?=$row['acc'];?></td>
+            <td><?=str_repeat('*',strlen($row['pw']));?></td>
+            <td>
+                <?php
+            if($row['acc']=='admin'){
+                echo "此帳號為最高權限，不可更改";
+            }else{
+                echo "<button onclick=location.href='?do=edit_admin&id={$row['id']}'>修改</button>";
+                echo "<button onclick=del('admin',{$row['id']})>刪除</button>";
+            }
+                ?>
+            </td>
+            
+        </tr>
+    <?php
+    }
+    ?>
+
 </table>
 <div class="ct">
     <button onclick="location.href='index.php'">返回</button>
