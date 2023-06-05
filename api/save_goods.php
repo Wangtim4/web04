cce8b3322cfe77bb041bb4d1b4e5b2a2f9737afc
a2453540 <a2453540@gmail.com>
2023-06-05 19:17:40 +0800

    完成商品新增架功能

--- api/save_goods.php
+++ api/save_goods.php
@@ -1,14 +1,19 @@
 <?php
 include_once "../base.php";
 
-// dd($_POST);
+dd($_POST);
 
-$_POST['no']=rand(100000,999999);
+
 if(isset($_FILES['img']['tmp_name'])){
     $_POST['img']=$_FILES['img']['tmp_name'];
     move_uploaded_file($_FILES['img']['tmp_name'],'../img/'.$_FILES['img']['name']);
 }
+// 判斷是否為新增，還是編輯
+if(!isset($_POST['id'])){
+ $_POST['no']=rand(100000,999999);
+    $_POST['sh']=1;
+}
 // dd($_POST);
 $Goods->save($_POST);
-to('../back.php?do=th')
+to('../back.php?do=th');
 ?>
