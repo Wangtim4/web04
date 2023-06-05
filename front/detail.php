99234a00d9b7d21f02a3954c1ef0c8980a9cb3ee
a2453540 <a2453540@gmail.com>
2023-06-05 16:33:43 +0800

    製作詳細資料的購買功能

--- front/detail.php
+++ front/detail.php
@@ -12,12 +12,23 @@ $good = $Goods->find($_GET['id']);
         <div>編號:<?=$good['no'];?></div>
         <div>價格:<?=$good['price'];?></div>
         <div>詳細說明:<?=$good['intro'];?></div>
+        <div>詳細說明:<?=$good['qt'];?></div>
     </div>
 </div>
 <div class="ct tt">
-    購買數量: <input type="number" name="qt" id="qt" value="qt">
+    購買數量: <input type="number" name="qt" id="qt" value='1'>
+    <a href="#" onclick="buycart()">
     <img src="./icon/0402.jpg" alt="">
+    </a>
 </div>
 <div class="ct">
     <button onclick="location.href='index.php'">返回</button>
 </div>
+
+<script>
+    function buycart(){
+        console.log("1");
+        let qt=$('#qt').val()
+        location.href=`?do=buycart&id=<?=$good['id'];?>&qt=${qt}`
+    }
+</script>
